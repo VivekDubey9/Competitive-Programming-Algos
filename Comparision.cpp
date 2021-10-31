f638ae0cd5fcc710e7702cd2a4dc63993fa9e357
Mantosh Sharma <56771403+mr-mantosh@users.noreply.github.com>
2021-10-31 23:06:51 +0530

    Comparison of float with a value in C

---
+++ Comparision.cpp
@@ -0,0 +1,15 @@
+include<stdio.h>
+int main()
+{
+    float x = 0.1;
+    if (x == 0.1)
+        printf("IF");
+    else if (x == 0.1f)
+        printf("ELSE IF");
+    else
+        printf("ELSE");
+}
+
+
+
+#outputof the program : ELSE IF
